d6456eae9a9c1662255d14e75c437632a50a1c56
Maxwell <maxwellherron5@gmail.com>
2021-02-21 14:51:46 -0600

    created spider for scraping captions

--- instascraper/instascraper/settings.py
+++ instascraper/instascraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'instascraper.spiders'
 #USER_AGENT = 'instascraper (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- instascraper/instascraper/spiders/instagram.py
+++ instascraper/instascraper/spiders/instagram.py
@@ -1,19 +1,109 @@
+import os
+
 import scrapy
-from urllib3.parse import urlencode
+from urllib.parse import urlencode
 import json
 from datetime import datetime
-API = 'YOURAPIKEY'
-user_accounts = ["dedszead"] 
+from dotenv import load_dotenv
 
-def start_requests(self):
-    for username in user_accounts:
-        url = f'https://www.instagram.com/{username}/?hl=en'
-        yield scrapy.Request(get_url(url), callback=self.parse)
+# Creating access to environment variables
+load_dotenv()
+
+API = os.getenv("API")
+user_accounts = ['dedszead']
 
 class InstagramSpider(scrapy.Spider):
     name = 'instagram'
-    allowed_domains = ['instagram.com']
-    start_urls = ['http://instagram.com/']
+    allowed_domains = ['api.webscraping.ai']
+    custom_settings = {'CONCURRENT_REQUESTS_PER_DOMAIN': 5}
+
+    def start_requests(self):
+        for username in user_accounts:
+            url = f'https://www.instagram.com/{username}/?hl=en'
+            yield scrapy.Request(self.get_url(url), callback=self.parse)
 
     def parse(self, response):
-        pass
+        x = response.xpath("//script[starts-with(.,'window._sharedData')]/text()").extract_first()
+        json_string = "{" + x.strip().split('= {')[1][:-1]
+        data = json.loads(json_string)
+        # all that we have to do here is to parse the JSON we have
+        user_id = data['entry_data']['ProfilePage'][0]['graphql']['user']['id']
+        next_page_bool = \
+            data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['page_info'][
+                'has_next_page']
+        edges = data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']
+        for i in edges:
+            url = 'https://www.instagram.com/p/' + i['node']['shortcode']
+            video = i['node']['is_video']
+            date_posted_timestamp = i['node']['taken_at_timestamp']
+            date_posted_human = datetime.fromtimestamp(date_posted_timestamp).strftime("%d/%m/%Y %H:%M:%S")
+            like_count = i['node']['edge_media_preview_like']['count'] if "edge_media_preview_like" in i['node'].keys() else ''
+            comment_count = i['node']['edge_media_to_comment']['count'] if 'edge_media_to_comment' in i[
+                'node'].keys() else ''
+            captions = ""
+            if i['node']['edge_media_to_caption']:
+                for i2 in i['node']['edge_media_to_caption']['edges']:
+                    captions += i2['node']['text'] + "\n"
+
+            if video:
+                image_url = i['node']['display_url']
+            else:
+                image_url = i['node']['thumbnail_resources'][-1]['src']
+            item = {'captions': captions[:-1]}
+            if video:
+                yield scrapy.Request(self.get_url(url), callback=self.get_video, meta={'item': item})
+            else:
+                item['videoURL'] = ''
+                yield item
+        if next_page_bool:
+            cursor = \
+                data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['page_info'][
+                    'end_cursor']
+            di = {'id': user_id, 'first': 12, 'after': cursor}
+            print(di)
+            params = {'query_hash': 'e769aa130647d2354c40ea6a439bfc08', 'variables': json.dumps(di)}
+            url = 'https://www.instagram.com/graphql/query/?' + urlencode(params)
+            yield scrapy.Request(self.get_url(url), callback=self.parse_pages, meta={'pages_di': di})
+
+    def parse_pages(self, response):
+        di = response.meta['pages_di']
+        data = json.loads(response.text)
+        for i in data['data']['user']['edge_owner_to_timeline_media']['edges']:
+            video = i['node']['is_video']
+            url = 'https://www.instagram.com/p/' + i['node']['shortcode']
+            if video:
+                image_url = i['node']['display_url']
+                video_url = i['node']['video_url']
+            else:
+                video_url = ''
+                image_url = i['node']['thumbnail_resources'][-1]['src']
+            date_posted_timestamp = i['node']['taken_at_timestamp']
+            captions = ""
+            if i['node']['edge_media_to_caption']:
+                for i2 in i['node']['edge_media_to_caption']['edges']:
+                    captions += i2['node']['text'] + "\n"
+            comment_count = i['node']['edge_media_to_comment']['count'] if 'edge_media_to_comment' in i['node'].keys() else ''
+            date_posted_human = datetime.fromtimestamp(date_posted_timestamp).strftime("%d/%m/%Y %H:%M:%S")
+            like_count = i['node']['edge_media_preview_like']['count'] if "edge_media_preview_like" in i['node'].keys() else ''
+            item = {'captions': captions[:-1]}
+            yield item
+        next_page_bool = data['data']['user']['edge_owner_to_timeline_media']['page_info']['has_next_page']
+        if next_page_bool:
+            cursor = data['data']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
+            di['after'] = cursor
+            params = {'query_hash': 'e769aa130647d2354c40ea6a439bfc08', 'variables': json.dumps(di)}
+            url = 'https://www.instagram.com/graphql/query/?' + urlencode(params)
+            yield scrapy.Request(self.get_url(url), callback=self.parse_pages, meta={'pages_di': di})
+
+    def get_video(self, response):
+        # only from the first page
+        item = response.meta['item']
+        video_url = response.xpath('//meta[@property="og:video"]/@content').extract_first()
+        item['videoURL'] = video_url
+        yield item
+    
+    @staticmethod
+    def get_url(url):
+        payload = {'api_key': API, 'proxy': 'residential', 'timeout': '20000', 'url': url}
+        proxy_url = 'https://api.webscraping.ai/html?' + urlencode(payload)
+        return proxy_url
